4ab186d7b4c4cd3c0a88fa50bb916c4ff87dfc89
Helmi Pradita <mr.hellmii@gmail.com>
2021-04-29 22:40:51 +0700

    menambahkan endpoint /get:id untuk menampilkan quotes by ID

--- routes/Quotes.js
+++ routes/Quotes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const Quote = require('../models/Quotes');
 
 //Get all routes
-router.get('/', (req, res) => {
-    res.send("Get all routes");
+router.get('/', async (req, res) => {
+    const quotes = await Quote.find();
+
+    res.json(quotes);
 });
 
 //Create new quote
@@ -15,5 +17,12 @@ router.post('/new', async (req, res) => {
 
     res.json(newQuote);
 });
+
+//Get specific quote
+router.get('/get/:id', async (req, res) => {
+    const q = await Quote.findById({ _id: req.params.id });
+
+    res.json(q);
+})
 
 module.exports = router;
